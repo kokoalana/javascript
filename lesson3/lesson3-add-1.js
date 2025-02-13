73d745f876493b4375a248b696be13dff376ab27
kokoalana <kokoa1lana@gmail.com>
2025-02-13 22:37:51 +0200

    js 3 lesson hw full

---
+++ lesson3/lesson3-add-1.js
@@ -0,0 +1,327 @@
+// #WpkK0ZH1
+
+// let arr = [1, 2, 3, 4, 5, 'краб', 'calendar', 'sauce', 'town', 'dragon', 6, 7, true, false, 'socket'];
+//
+// console.log(arr);
+//
+// #4aDbSgh
+//
+// let empty = [];
+// empty[0] = 'birthday';
+// empty[1] = 'mama';
+// empty[2] = false;
+// empty[3] = true;
+// empty[4] = 45;
+//
+// console.log(empty);
+//
+// #qLQLJSeN7i
+//
+// let existarr = [2,17,13,6,22,31,45,66,100,-18];
+
+// 1
+// let i = 0;
+// while (i < existarr.length) {
+//     console.log(existarr[i]);
+//     i++;
+// }
+
+// 2
+// for (let index = 0; index < existarr.length; index++) {
+//     console.log(existarr[index]);
+// }
+
+// 3
+// let i = 0;
+// while (i < existarr.length) {
+//     if (i % 2 !== 0) {
+//         console.log(existarr[i]);
+//     }
+//     i++;
+// };
+
+// 4
+// for (let i = 0; i < existarr.length; i++) {
+//     if (i % 2 !== 0) {
+//         console.log(existarr[i]);
+//     }
+// }
+
+// 5
+// let i = 0;
+// while (i < existarr.length) {
+//     if (i % 2 === 0) {
+//         console.log(existarr[i]);
+//     }
+//     i++;
+// }
+
+// 6
+// for (let i = 0; i < existarr.length; i++) {
+//     if (i % 2 === 0) {
+//         console.log(existarr[i]);
+//     }
+// }
+
+// 7
+// for (let i = 1; i < existarr.length; i++) {
+//     if (i % 3 === 0) {
+//         existarr[i] = 'okten';
+//     }
+//     console.log(existarr[i])
+// }
+
+// 8
+// for (let i = existarr.length - 1; i >= 0; i--) {
+//     console.log(existarr[i]);
+// }
+
+// 1[2]
+// let i = existarr.length - 1;
+// while (i >= 0) {
+//     console.log(existarr[i]);
+//     i--;
+// }
+
+// 2[2]
+// for (let index = existarr.length - 1; index >= 0; index--) {
+//     console.log(existarr[index]);
+// }
+
+// 3[2]
+// let i = existarr.length - 1;
+// while (i >= 0) {
+//     if (i % 2 !== 0) {
+//         console.log(existarr[i]);
+//     }
+//     i--;
+// };
+
+// 4[2]
+// for (let i = existarr.length - 1; i >= 0; i--) {
+//     if (i % 2 !== 0) {
+//         console.log(existarr[i]);
+//     }
+// }
+
+// 5[2]
+// let i = existarr.length - 1;
+// while (i >= 0) {
+//     if (i % 2 === 0) {
+//         console.log(existarr[i]);
+//     }
+//     i--;
+// }
+
+// 6[2]
+// for (let i = existarr.length - 1; i >= 0; i--) {
+//     if (i % 2 === 0) {
+//         console.log(existarr[i]);
+//     }
+// }
+
+// 7[2]
+// for (let i = existarr.length - 1; i >= 0; i--) {
+//     if (i % 3 === 0) {
+//         existarr[i] = 'okten';
+//     }
+//     console.log(existarr[i])
+// }
+
+// #yHAwJOyiC
+
+// let arrNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//
+// for (let iNum = 0; iNum < arrNum.length; iNum++) {
+//     console.log(arrNum[iNum]);
+// }
+
+// #GamKju89ob
+
+// let arrString = ['milf', 'book', 'pony', 'sea', 'kiwi', 'pine', 'cacao', 'fireplace', 'whale', 'bucket']
+//
+// for (let iString = 0; iString < arrString.length; iString++) {
+//     console.log(arrString[iString]);
+// }
+
+// #Bm76xmg
+
+// let arrAny = ['milf', 1, 'book', -2, false, 'pony', 3.14, true, 'cacao', 'kiwi'];
+//
+// for (let iAny = 0; iAny < arrAny.length; iAny++) {
+//     console.log(arrAny[iAny]);
+// }
+
+// #u3vmD0YJXh
+// піддивилась відповідь в чата GPT, бо консоль нічого не видавала.
+
+// let arrAny = ['milf', 1, 'book', -2, false, 'pony', 3.14, true, 'cacao', 'kiwi'];
+//
+// for (let i = 0; i < arrAny.length; i++) {
+//     if (typeof arrAny[i] === 'boolean') {
+//         console.log(arrAny[i]);
+//     }
+// }
+
+// #9stMq2ou
+
+// let arrAny = ['milf', 1, 'book', -2, false, 'pony', 3.14, true, 'cacao', 'kiwi'];
+//
+// for (let i = 0; i < arrAny.length; i++) {
+//     if (typeof arrAny[i] === 'number') {
+//         console.log(arrAny[i]);
+//     }
+// }
+
+// #mK4pmM4
+
+// let arrAny = ['milf', 1, 'book', -2, false, 'pony', 3.14, true, 'cacao', 'kiwi'];
+//
+// for (let i = 0; i < arrAny.length; i++) {
+//     if (typeof arrAny[i] === 'string') {
+//         console.log(arrAny[i]);
+//     }
+// }
+
+// #0pm3EyTKy9
+
+// let emptyArr = [];
+//
+// emptyArr[0] = 12;
+// emptyArr[1] = true;
+// emptyArr[2] = 'Sherlock';
+// emptyArr[3] = false;
+// emptyArr[4] = -156,99;
+// emptyArr[5] = 0;
+// emptyArr[6] = 'iqos';
+// emptyArr[7] = 'help';
+// emptyArr[8] = 105;
+// emptyArr[9] = 'and';
+//
+// for (i = 0; i < emptyArr.length; i++) {
+//     console.log(emptyArr[i]);
+// }
+
+// #mDMWMW5a
+
+// for (i = 1; i <= 10; i++) {
+//     console.log(i);
+//     document.write(i + '-');
+// }
+
+// #4sXhaa5YMM
+
+// for (i = 0; i < 100; i++) {
+//     console.log(i);
+//     document.write(i + '-');
+// }
+
+// #s24slNyz7
+
+// for (i = 0; i < 100; i += 2) {
+//     document.write(i + '-');
+//     console.log(i);
+// }
+
+// #zananT5FR1
+
+// for (i = 0; i < 100; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//         document.write(i)
+//     }
+// }
+
+// #Tfrwls7FM
+
+// for (i = 0; i < 100; i++) {
+//     if (i % 2 !== 0) {
+//         console.log(i);
+//         document.write(i)
+//     }
+// }
+
+// #reLkOkTB29Q
+
+let books = [
+
+    {name: 'The Grass Harp',
+    pageCount: 150,
+    author: ['Truman Capote'],
+    genre: ['drama', 'realism', 'southern literature']},
+
+    {name: 'Watership Dawn',
+    pageCount: 413,
+    author: ['Richard Addams'],
+    genre: ['fantasy', 'fairytale', 'animal adventure', 'drama']},
+
+    {name: 'Good Omens',
+    pageCount: 432,
+    author: ['Neil Gaiman', 'Terry Pratchett'],
+    genre: ['fantasy', 'comedy', 'religious', 'satiric fiction', 'apocaliptic fiction']},
+
+    {name: 'The Wind in the Willows',
+    pageCount: 250,
+    author: ['Kenneth Grahame'],
+    genre: ['child literature', 'fantasy', 'action']}
+]
+
+
+// let biggestBook = books[0].name;
+// let biggestBookByPageCount = books[0].pageCount;
+//
+// for (let book of books) {
+//     if (book.pageCount > biggestBookByPageCount) {
+//         biggestBookByPageCount = book.pageCount;
+//         biggestBook = book.name;
+//     }
+// }
+//
+// console.log(biggestBook);
+
+
+// let biggestByGenre = books[0].name;
+// let moreGenres = books[0].genre.length;
+//
+// for (let book of books) {
+//     if (book.genre.length > moreGenres) {
+//         moreGenres = book.genre.length;
+//         biggestByGenre = book.name;
+//     }
+// }
+//
+// console.log(biggestByGenre);
+
+// let longestTitleBook = books[0].name;
+// let biggestByTitle = books[0].name.length;
+//
+// for (let book of books) {
+//     if (book.name.length > biggestByTitle) {
+//         biggestTitleBook = book.name.length;
+//         longestTitleBook = book.name;
+//     }
+// }
+//
+// console.log(longestTitleBook);
+
+// let twoAuthorsBook = books[0].name;
+// let biggestByAuthors = books[0].author.length;
+//
+// for (let book of books) {
+//     if (book.author.length === 2) {
+//         biggestByAuthors = book.author.length;
+//         twoAuthorsBook = book.name;
+//     }
+// }
+//
+// console.log(twoAuthorsBook);
+
+let oneAuthorBook = [];
+
+for (let book of books) {
+    if (book.author.length === 1) {
+        oneAuthorBook.push(book.name);
+    }
+}
+
+console.log(oneAuthorBook);
